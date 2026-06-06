51eaee32be65961bf573e5a8288f560235f626b9
maksimkk6 <konnovmaks@bk.ru>
2026-06-06 14:35:27 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,53 +1,36 @@
 // Copyright 2022 NNTU-CS
-#ifndef TREE_H
-#define TREE_H
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
+
 #include <vector>
+
+struct PMNode {
+  char value;
+  std::vector<PMNode*> children;
+
+  explicit PMNode(char val = '\0') : value(val) {}
+};
+
 class PMTree {
- public:
-  struct Node {
-    char value;
-    std::vector<Node*> childs;
-    Node(char v = 0) : value(v) {}
-  };
-  Node* root;
-  int alphabetSize;
-
-  explicit PMTree(const std::vector<char>& data) {
-    root = new Node(0);
-    alphabetSize = static_cast<int>(data.size());
-    build(root, data);
-  }
-  ~PMTree() {
-    clear(root);
-  }
  private:
-  void build(Node* parent, const std::vector<char>& symbols) {
-    if (symbols.empty())
-      return;
-    for (size_t i = 0; i < symbols.size(); i++) {
-      Node* node = new Node(symbols[i]);
-      parent->childs.push_back(node);
-      std::vector<char> next;
-      for (size_t j = 0; j < symbols.size(); j++)
-        if (j != i)
-          next.push_back(symbols[j]);
-      build(node, next);
-    }
-  }
+  PMNode* root_;
+  std::vector<char> alphabet_;
 
-  void clear(Node* node) {
-    if (!node)
-      return;
+  void buildTree(PMNode* node, const std::vector<char>& elems);
+  void deleteTree(PMNode* node);
 
-    for (auto x : node->childs)
-      clear(x);
+ public:
+  explicit PMTree(const std::vector<char>& elems);
+  ~PMTree();
 
-    delete node;
-  }
+  PMNode* getRoot() const;
+  const std::vector<char>& getAlphabet() const;
 };
 
 std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+
 std::vector<char> getPerm1(PMTree& tree, int num);
+
 std::vector<char> getPerm2(PMTree& tree, int num);
 
-#endif
+#endif  // INCLUDE_TREE_H_
