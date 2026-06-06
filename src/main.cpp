ce072b2d04969131456fc6e9c42fe19c071104d0
maksimkk6 <konnovmaks@bk.ru>
2026-06-06 14:39:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
+#include <vector>
 #include "tree.h"
-
 int main() {
+  std::vector<char> in = {'1', '2', '3'};
+  PMTree tree(in);
+  std::cout << "Perm #1 (getPerm1): ";
+  auto p1 = getPerm1(tree, 1);
+  for (char c : p1) {
+    std::cout << c;
+  }
+  std::cout << std::endl;
+  std::cout << "Perm #2 (getPerm2): ";
+  auto p2 = getPerm2(tree, 2);
+  for (char c : p2) {
+    std::cout << c;
+  }
+  std::cout << std::endl;
+
   return 0;
 }
